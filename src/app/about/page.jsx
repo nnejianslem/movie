64e53968753e1059b4d917a16c0eebaa79977bed
nnejianslem <151783520+nnejianslem@users.noreply.github.com>
2024-10-13 12:36:06 +0100

    add the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptas at nostrum quidem eligendi, aliquam voluptatum adipisci corrupti dolore atque id beatae fugiat porro qui odio? Vero beatae laborum, quisquam voluptate velit, perferendis nihil laudantium voluptatem reprehenderit impedit consequuntur? Voluptates, asperiores obcaecati. Doloribus quis praesentium dolorem, tempore quae quaerat officiis?</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus atque laudantium inventore nihil nesciunt enim necessitatibus ex temporibus architecto.</p>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, odio? Labore quaerat architecto eveniet delectus, nostrum tenetur temporibus rem omnis, eligendi, eos accusamus neque blanditiis mollitia unde provident natus deserunt dolorum eaque repellendus necessitatibus. Sapiente sed perspiciatis inventore similique corrupti dolore velit impedit minus, eaque dolorum delectus. Amet, nihil nam nisi at reprehenderit placeat ab facere. Eveniet velit quam laborum eum corrupti autem cupiditate reiciendis nostrum vero eos. Dicta eum expedita perspiciatis ratione! Nostrum numquam sequi cupiditate in, similique iure voluptatum laborum, nulla necessitatibus, aliquam consectetur ducimus? Quasi neque numquam quam facilis autem. Veniam consectetur obcaecati aliquam repudiandae. Dolorum, sit?</p>
+    </div>
+  )
+}
+
